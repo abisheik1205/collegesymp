6587a414f04fe971060227cd988aa2547fea0aa0
Arul Rosario <itsarrowhere380@gmail.com>
2023-09-15 22:39:17 +0530

    Corrections Made v12

--- src/Components/Home/AboutUs/AboutUs.jsx
+++ src/Components/Home/AboutUs/AboutUs.jsx
@@ -17,7 +17,7 @@ const AboutUs = () => {
                             <span className="tobehidden"><p className="aboutUsContentPara">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; KCG College is committed to fostering responsible citizenship and progressive leadership by focusing on students' overall development and strengthening their leadership abilities. They continually adapt their curriculum to include value-added programs, enhancing students' job prospects while aligning their offerings with industry needs. With numerous undergraduate and postgraduate programs, accreditation by NBA, and recognition as a research center by Anna University, KCG College of Technology stands as a symbol of quality education and industry partnership in the field of engineering and technology.</p></span>
                         </div>
                         <div className="collegeqoute">
-                            <p className="collegeqoutePara">To Make Every Man A Success And No Man A Failure <span className="collegeqouteParaBold">&nbsp;-&nbsp;  KCG Verghese</span></p>
+                            <p className="collegeqoutePara">To Make Every Man A Success And No Man A Failure <span className="collegeqouteParaBold">&nbsp;-&nbsp; Dr KCG Verghese</span></p>
                         </div>
                         <div className="linkonaboutus">
                             <a href="https://kcgcollege.ac.in/" target="_blank" rel="noreferrer"><p className="linkFoundOnAboutUs">Website</p></a>
